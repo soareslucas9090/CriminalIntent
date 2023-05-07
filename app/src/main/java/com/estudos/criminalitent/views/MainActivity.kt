96f2f5905efcbd908144e403e65da897fe795366
Lucas Soares <soareslukas9090@gmail.com>
2023-05-07 19:33:32 -0300

    Initial commit

---
+++ app/src/main/java/com/estudos/criminalitent/views/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.estudos.criminalitent.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.estudos.criminalitent.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
