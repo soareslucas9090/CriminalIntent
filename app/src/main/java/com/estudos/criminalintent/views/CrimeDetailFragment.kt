d7b0222c1b9a0d6df9254e0e6cf897c70e51ad7e
Lucas Soares <soareslukas9090@gmail.com>
2023-06-15 10:30:05 -0300

    Começo das implementações dos fragments

--- app/src/main/java/com/estudos/criminalintent/views/CrimeDetailFragment.kt
+++ app/src/main/java/com/estudos/criminalintent/views/CrimeDetailFragment.kt
@@ -28,7 +28,7 @@ class CrimeDetailFragment: Fragment() {
         )
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentCrimeDetailBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
